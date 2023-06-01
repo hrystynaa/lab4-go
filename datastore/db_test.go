4121e912021be3f7a292c0d923faa761197cbf0f
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2023-06-01 11:35:37 +0300

    Add tests for segmentation

--- datastore/db_test.go
+++ datastore/db_test.go
@@ -20,7 +20,7 @@ func TestDb_Put(t *testing.T) {
 	}
 	defer db.Close()
 
-	pairs := [][]string {
+	pairs := [][]string{
 		{"key1", "value1"},
 		{"key2", "value2"},
 		{"key3", "value3"},
@@ -64,7 +64,7 @@ func TestDb_Put(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if size1 * 2 != outInfo.Size() {
+		if size1*2 != outInfo.Size() {
 			t.Errorf("Unexpected size (%d vs %d)", size1, outInfo.Size())
 		}
 	})
@@ -88,5 +88,56 @@ func TestDb_Put(t *testing.T) {
 			}
 		}
 	})
+}
+func TestDb_Segmentation(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test-db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
 
+	db, err := NewDb(dir, 150)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	t.Run("new file", func(t *testing.T) {
+
+		err = db.Put("key1", "value11")
+		err = db.Put("key2", "value21")
+		err = db.Put("key1", "value12")
+		err = db.Put("key2", "value22")
+		err = db.Put("key3", "value31")
+
+		if len(db.segments) != 2 {
+			t.Errorf("Expected 2 segments, got %d", len(db.segments))
+		}
+	})
+
+	t.Run("segmentation", func(t *testing.T) {
+		err = db.Put("key1", "value13")
+		err = db.Put("key3", "value32")
+
+		if len(db.segments) != 3 {
+			t.Errorf("Expected 3 segments, got %d", len(db.segments))
+		}
+
+		time.Sleep(3 * time.Second)
+
+		if len(db.segments) != 2 {
+			t.Errorf("Expected 2 segments, got %d", len(db.segments))
+		}
+	})
+
+	t.Run("delete old values", func(t *testing.T) {
+		value, _ := db.Get("key1")
+		if value != "value13" {
+			t.Errorf("Bad value returned expected value22, got %s", value)
+		}
+		value1, _ := db.Get("key2")
+		if value1 != "value22" {
+			t.Errorf("Bad value returned expected value22, got %s", value1)
+		}
+	})
 }
